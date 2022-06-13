b046893a2c3a34bbe34019bb5afcf01b35e8b66a
Ben <ben.marten@me.com>
2022-06-13 13:34:49 -0400

    add typescript types

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function getSmoothedArrayMulti(array: number[], iterations?: number, window?: number, edge?: number, round?: boolean): number[];
+export declare function getSmoothedArrayIgnoringNull(array: number[], window?: number, edge?: number, round?: boolean): number[];
+export declare function getSmoothedArray(array: number[], window?: number, edge?: number, round?: boolean): number[];
